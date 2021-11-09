20be8063037f890d96d02e4f0df233314a35f147
Franco <franco.mana@hotmail.com>
2021-11-09 18:04:32 -0300

    Main Menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+
+    void Update()
+    {
+        
+    }
+
+    public void PlayerVSAI()
+    {
+        SceneManager.LoadScene("PlayerVSAI");
+    }
+
+    public void PlayerVSPlayer()
+    {
+        SceneManager.LoadScene("PlayerVSPlayer");
+    }
+}
